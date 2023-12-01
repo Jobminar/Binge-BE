24ff8e622481dbcaf4f4f6750940657acca5e26b
chandra8120 <chandrasekhar8120@gmail.com>
2023-12-01 14:38:48 +0530

    add the data14

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,12 @@
+// userController.js
+
 import User from "../models/userModel.js";
 
 const userController = {
-
   createUser: async (req, res) => {
     try {
-      const user = new User(req.body);
+      const { sNo, name, whatsappNumber, email, numberOfPeople, eventType, date } = req.body;
+      const user = new User({ sNo, name, whatsappNumber, email, numberOfPeople, eventType, date });
       await user.save();
       res.status(201).json(user);
     } catch (error) {
@@ -12,20 +14,17 @@ const userController = {
     }
   },
 
-
-getUsers: async (req, res) => {
-  try {
-    console.log("Fetching users...");
-    const users = await User.find();
-    console.log("Users:", users);
-    res.status(200).json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: error.message });
-  }
-},
-
-
+  getUsers: async (req, res) => {
+    try {
+      console.log("Fetching users...");
+      const users = await User.find();
+      console.log("Users:", users);
+      res.status(200).json(users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 
 export default userController;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,14 @@
+
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
+  sNo: { type: Number, required: true },
   name: { type: String, required: true },
   whatsappNumber: { type: String, required: true },
   email: { type: String, required: true },
   numberOfPeople: { type: Number, required: true },
   eventType: { type: String, required: true },
+  date: { type: Date, required: true },
 });
 
 const User = model("User", userSchema);
